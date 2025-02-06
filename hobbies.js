5cf14fa2c1981d121f157cb5fbf47f75d3582b82
“CarmeloZiino” <carmeloziino.dev@gmail.com>
2025-02-06 12:34:13 +0100

    Creazione Repo e Completamento names.js e hobbies.js

---
+++ hobbies.js
@@ -0,0 +1,16 @@
+/*
+2 - hobbies.js
+1. Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire  un array di stringhe contenenti gli hobby.
+2. Esporta la funzione dal file.*/
+
+
+
+
+function hobbiesFunction ( hobbyOne , hobbyTwo , hobbyThree){
+
+   return [hobbyOne, hobbyTwo, hobbyThree];
+    console.log (hobby)
+};
+
+console.log (hobbiesFunction ("Calcio", "Tennis", "Gaming"))
+
---
+++ names.js
@@ -0,0 +1,12 @@
+/*  names.js
+1. Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+2. Esporta la funzione dal file.*/
+
+function namesFunctions ( firstName , lastName){
+
+ return {
+    "nome": firstName,
+    "cognome": lastName}
+};
+
+console.log(namesFunctions("Carmelo" , "Ziino"))
